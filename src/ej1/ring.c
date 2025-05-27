78d10e1011cfae73bb474efdf4d2b95bbb9e7e98
micmich05 <127415738+micmich05@users.noreply.github.com>
2025-05-27 15:56:35 -0300

    what

--- src/ej1/ring.c
+++ src/ej1/ring.c
@@ -24,31 +24,30 @@ int main(int argc, char **argv)
     int total = n + 1; // padre + n hijos
     // matriz de pipes: pipes[i] conecta ring_procs[i] -> ring_procs[(i+1)%(n+1)]
     int (*pipes)[2] = malloc(sizeof(int[2]) * total);
-    // if (!pipes) { perror("malloc"); exit(EXIT_FAILURE); }
+    if (!pipes) { perror("malloc"); exit(EXIT_FAILURE); }
 
-    // for (int i = 0; i < total; ++i) {
-    //     if (pipe(pipes[i]) == -1) {
-    //         perror("pipe");
-    //         exit(EXIT_FAILURE);
-    //     }
-    // }
+    for (int i = 0; i < total; ++i) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     /* Fork de los n hijos */
     int myid = -1;
     for (int i = 0; i < n; ++i) {
         pid = fork();
-        // if (pid < 0) {
-        //     perror("fork");
-        //     exit(EXIT_FAILURE);
-        // }
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
         if (pid == 0) {
             myid = i;  // en el hijo, guardo su índice
             break;
         }
         // el padre sigue creando más hijos
     }
-
-    //manejo de indices
+    
     int ring_idx;
     if (pid > 0) {
         // estamos en el padre
@@ -77,20 +76,18 @@ int main(int argc, char **argv)
     if (ring_idx == 0) {
         /* === PADRE === */
         // envío el mensaje inicial al hijo 'start'
-        // if (write(pipes[0][1], &buffer[0], sizeof(buffer[0])) != sizeof(buffer[0])) {
-        //     perror("write padre");
-        //     exit(EXIT_FAILURE);
-        // }
-        write(pipes[0][1], &buffer[0], sizeof(buffer[0]));
+        if (write(pipes[0][1], &buffer[0], sizeof(buffer[0])) != sizeof(buffer[0])) {
+            perror("write padre");
+            exit(EXIT_FAILURE);
+        }
         close(pipes[0][1]);
 
         // recibo el resultado final
         int result;
-        // if (read(pipes[total-1][0], &result, sizeof(result)) != sizeof(result)) {
-        //     perror("read padre");
-        //     exit(EXIT_FAILURE);
-        // }
-        read(pipes[total-1][0], &result, sizeof(result));
+        if (read(pipes[total-1][0], &result, sizeof(result)) != sizeof(result)) {
+            perror("read padre");
+            exit(EXIT_FAILURE);
+        }
         close(pipes[total-1][0]);
 
         printf("Resultado final: %d\n", result);
@@ -103,20 +100,18 @@ int main(int argc, char **argv)
         /* === HIJO === */
         int recv;
         int prev = (ring_idx - 1 + total) % total;
-        // if (read(pipes[prev][0], &recv, sizeof(recv)) != sizeof(recv)) {
-        //     perror("read hijo");
-        //     exit(EXIT_FAILURE);
-        // }
-        read(pipes[prev][0], &recv, sizeof(recv));
+        if (read(pipes[prev][0], &recv, sizeof(recv)) != sizeof(recv)) {
+            perror("read hijo");
+            exit(EXIT_FAILURE);
+        }
         close(pipes[prev][0]);
 
         // incremento y paso al siguiente
         recv++;
-        // if (write(pipes[ring_idx][1], &recv, sizeof(recv)) != sizeof(recv)) {
-        //     perror("write hijo");
-        //     exit(EXIT_FAILURE);
-        // }
-        write(pipes[ring_idx][1], &recv, sizeof(recv));
+        if (write(pipes[ring_idx][1], &recv, sizeof(recv)) != sizeof(recv)) {
+            perror("write hijo");
+            exit(EXIT_FAILURE);
+        }
         close(pipes[ring_idx][1]);
 
         exit(EXIT_SUCCESS);
